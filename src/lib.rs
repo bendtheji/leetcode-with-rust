28ce949874a0163d2dec0087a6fc14d926d4752e
bendtheji <benj.soon@gmail.com>
2024-02-25 19:06:30 +0800

    p205 isomorphic strings

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ mod bst_util;
 mod p997_find_the_town_judge;
 mod p242_valid_anagram;
 mod p150_evaluate_reverse_polish_notation;
+mod p205_isomorphic_strings;
---
+++ src/p205_isomorphic_strings.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+
+use std::collections::{HashMap, HashSet};
+
+pub fn is_isomorphic(s: String, t: String) -> bool {
+    let mut used = HashSet::new();
+    let mut map = HashMap::new();
+
+    for (c1, c2) in s.chars().zip(t.chars()) {
+        match map.get(&c1) {
+            Some(&c) => {
+                if c != c2 {
+                    return false;
+                }
+            },
+            None => {
+                if used.contains(&c2) { return false; }
+                used.insert(c2);
+                map.insert(c1, c2);
+            },
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::p205_isomorphic_strings::is_isomorphic;
+
+    #[test]
+    fn test_one() {
+        assert!(is_isomorphic("egg".to_string(), "add".to_string()));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_two() {
+        assert!(is_isomorphic("foo".to_string(), "bar".to_string()));
+    }
+
+    #[test]
+    fn test_three() {
+        assert!(is_isomorphic("paper".to_string(), "title".to_string()));
+    }
+}
